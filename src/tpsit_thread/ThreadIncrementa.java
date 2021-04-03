3c91578f6dfe4f95fbc91c794f5e54fbe1d1bf3b
Alessandro <alessandro.divincenzo77@gmail.com>
2021-04-03 13:21:31 +0200

    Primo commit

---
+++ src/tpsit_thread/ThreadIncrementa.java
@@ -0,0 +1,21 @@
+package tpsit_thread;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ThreadIncrementa implements Runnable {
+
+    Counter contatore = new Counter();
+    
+    @Override
+    public void run(){
+        for(int i = 0; i < 1000; i++){
+            contatore.incrementa();
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(ThreadIncrementa.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+    
+}
